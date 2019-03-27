17a148c5b98367c48c318254bb5f24f6e2d3f408
Paul <paul.fleming@nrel.gov>
2019-03-26 21:23:12 -0600

    adding code to track 2 new repos

--- get_stats.py
+++ get_stats.py
@@ -5,6 +5,7 @@ import database as dbc
 
 db = dbc.Database()
 
+# WISDEM first
 traffic = ghs.RepoTraffic()
 views = traffic.get_views()
 clones = traffic.get_clones()
@@ -14,3 +15,25 @@ if clones['clones']:
 
 if views['views']:
     db.addViews(views['views'])
+
+# NREL/FLORIS 
+traffic = ghs.RepoTraffic(owner='NREL', repository='floris')
+views = traffic.get_views()
+clones = traffic.get_clones()
+
+if clones['clones']:
+    db.addClones(clones['clones'],repo_code='nrel_floris')
+
+if views['views']:
+    db.addViews(views['views'],repo_code='nrel_floris')
+
+# wfc_tools 
+traffic = ghs.RepoTraffic(owner='NREL', repository='wfc_tools')
+views = traffic.get_views()
+clones = traffic.get_clones()
+
+if clones['clones']:
+    db.addClones(clones['clones'],repo_code='wfc_tools')
+
+if views['views']:
+    db.addViews(views['views'],repo_code='wfc_tools')
