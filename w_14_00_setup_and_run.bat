cc20d474facca27831608de27b55dacd9f59d264
Paul <paul.fleming@nrel.gov>
2019-12-06 15:03:21 -0700

    add was 14 scripts

---
+++ w_14_00_setup_and_run.bat
@@ -0,0 +1,1 @@
+start cmd /k  C:\Users\pfleming\Desktop\ghstats\ghstats\w_14_00_setup_and_run_sub.bat
---
+++ w_14_00_setup_and_run_sub.bat
@@ -0,0 +1,6 @@
+set PATH=C:\ProgramData\Anaconda3\;C:\ProgramData\Anaconda3\Scripts;c:\ProgramData\anaconda3\Library\bin;%PATH%
+C:\ProgramData\Anaconda3\Scripts\conda config --set ssl_verify no
+call C:\ProgramData\Anaconda3\Scripts\activate field3
+cd C:\Users\pfleming\Desktop\ghstats\ghstats
+git pull
+python -u get_stats.py
